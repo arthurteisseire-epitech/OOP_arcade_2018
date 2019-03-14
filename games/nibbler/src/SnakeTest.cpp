d97c8caf5f3026748832033e1185e5ad04e6ebd0
Arthamios <adrien.fabre@epitech.eu>
2019-03-14 21:51:48 +0100

    Test Snake init.

--- games/nibbler/src/SnakeTest.cpp
+++ games/nibbler/src/SnakeTest.cpp
@@ -11,8 +11,26 @@
 
 class SnakeTest : public ::testing::Test, public arc::Snake {
 public:
-	SnakeTest() : Snake({1, 2}, 4) {};
+	SnakeTest() : Snake(init_pos, 4) {};
+
+protected:
+	static const std::pair<unsigned int, unsigned int> init_pos;
+	static const std::pair<unsigned int, unsigned int> move;
 };
+
+const std::pair<unsigned int, unsigned int> SnakeTest::init_pos = {1, 2};
+
+TEST_F(SnakeTest, Init)
+{
+	EXPECT_EQ(_body_positions.size(), 4);
+	EXPECT_EQ(_body_positions[0], init_pos);
+	EXPECT_EQ(_body_positions[1].first, init_pos.first + 1);
+	EXPECT_EQ(_body_positions[1].second, init_pos.second);
+	EXPECT_EQ(_body_positions[2].first, init_pos.first + 2);
+	EXPECT_EQ(_body_positions[2].second, init_pos.second);
+	EXPECT_EQ(_body_positions[3].first, init_pos.first + 3);
+	EXPECT_EQ(_body_positions[3].second, init_pos.second);
+}
 
 TEST_F(SnakeTest, Eat)
 {
