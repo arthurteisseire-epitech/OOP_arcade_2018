2c60777db02182bc8a771af3615fbc7f8a363dcc
Arthamios <adrien.fabre@epitech.eu>
2019-03-14 22:03:13 +0100

    Add SnakeTest find tail direction.

--- games/nibbler/src/SnakeTest.cpp
+++ games/nibbler/src/SnakeTest.cpp
@@ -43,6 +43,25 @@ TEST_F(SnakeTest, Eat)
 	EXPECT_EQ(cp.back().second, _body_positions.back().second);
 }
 
+TEST_F(SnakeTest, FindTailDirection)
+{
+	_body_positions.push_back(pos_t(0, 2));
+	_body_positions.push_back(pos_t(0, 3));
+	EXPECT_EQ(find_tail_direction(), pos_t(0, -1));
+
+	_body_positions.push_back(pos_t(0, 3));
+	_body_positions.push_back(pos_t(0, 2));
+	EXPECT_EQ(find_tail_direction(), pos_t(0, 1));
+
+	_body_positions.push_back(pos_t(2, 0));
+	_body_positions.push_back(pos_t(3, 0));
+	EXPECT_EQ(find_tail_direction(), pos_t(-1, 0));
+
+	_body_positions.push_back(pos_t(3, 0));
+	_body_positions.push_back(pos_t(2, 0));
+	EXPECT_EQ(find_tail_direction(), pos_t(1, 0));
+}
+
 //TEST_F(SnakeTest, Move)
 //{
 //	std::list<std::pair<unsigned int, unsigned int>> pos_list = _;
