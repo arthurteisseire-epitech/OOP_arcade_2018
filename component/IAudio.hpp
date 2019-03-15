88d0900965f1419966afb8ab0a46f4590bd91d7c
arthur <arthur.teisseire@epitech.eu>
2019-03-15 17:23:34 +0100

    Add IAudio to components

---
+++ component/IAudio.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_arcade_2018
+** File description:
+** Created by abel,
+*/
+
+#ifndef OOP_ARCADE_2018_I_AUDIO_HPP
+#define OOP_ARCADE_2018_I_AUDIO_HPP
+
+#include <string>
+#include "IComponent.hpp"
+
+namespace arc {
+
+    class IAudio : public IComponent {
+        public:
+
+            /**
+             * @return the audio volume between 0 and 100
+             */
+            virtual int getVolume() const = 0;
+
+            /**
+             * @return the relative audio file path to be loaded
+             */
+            virtual const std::string &getSoundPath() const = 0;
+    };
+}
+
+#endif
