54d3c07e0b1b9eed9cda80b09d1bb03a3a1a3d77
arthur <arthur.teisseire@epitech.eu>
2019-03-15 17:28:34 +0100

    Reformat IAudio to style

--- component/IAudio.hpp
+++ component/IAudio.hpp
@@ -13,19 +13,19 @@
 
 namespace arc {
 
-    class IAudio : public IComponent {
-        public:
+	class IAudio : public IComponent {
+	public:
 
-            /**
-             * @return the audio volume between 0 and 100
-             */
-            virtual int getVolume() const = 0;
+		/**
+		 * @return the audio volume between 0 and 100
+		 */
+		virtual int getVolume() const = 0;
 
-            /**
-             * @return the relative audio file path to be loaded
-             */
-            virtual const std::string &getSoundPath() const = 0;
-    };
+		/**
+		 * @return the relative audio file path to be loaded
+		 */
+		virtual const std::string &getSoundPath() const = 0;
+	};
 }
 
 #endif
