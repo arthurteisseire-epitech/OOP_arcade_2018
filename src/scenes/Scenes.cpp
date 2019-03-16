6fcc3c939526591242a65bc3b77f07605e9f0a1d
arthur <arthur.teisseire@epitech.eu>
2019-03-16 12:10:21 +0100

    Add Scenes class

---
+++ src/scenes/Scenes.cpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** Scenes.cpp
+*/
+
+#include "Scenes.hpp"
+
---
+++ src/scenes/Scenes.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** Scenes.hpp
+*/
+
+#ifndef ARCADE_SCENES_HPP
+#define ARCADE_SCENES_HPP
+
+namespace arc {
+	class Scenes {
+	};
+}
+
+#endif
