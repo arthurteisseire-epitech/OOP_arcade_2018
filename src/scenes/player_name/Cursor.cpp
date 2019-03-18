315fd507b9ea2554ee68316d611ac33833fd92b3
arthur <arthur.teisseire@epitech.eu>
2019-03-18 21:18:07 +0100

    Cursor is now set as expected in PlayerName

--- src/scenes/player_name/Cursor.cpp
+++ src/scenes/player_name/Cursor.cpp
@@ -11,13 +11,16 @@ arc::Cursor::Cursor(Text *focus) :
 	Sprite(""),
 	_focus(focus)
 {
-	setSize(std::pair<float, float>(0.02, 0.005));
-	setPosition(focus->getPosition());
+	setSize(std::pair<float, float>(0.02, 0.04));
+	changeFocus(focus);
 }
 
 void arc::Cursor::changeFocus(arc::Text *newFocus)
 {
-	setPosition(newFocus->getPosition());
+	std::pair<float, float> textPos(newFocus->getPosition().first - getSize().first / 2.0,
+	                                newFocus->getPosition().second - getSize().second / 2.0);
+
+	setPosition(textPos);
 	_focus = newFocus;
 }
 
