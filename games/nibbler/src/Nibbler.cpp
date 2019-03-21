c8a9317642428dc1c8b2f3d28bd28a7a735d6b72
Arthamios <adrien.fabre@epitech.eu>
2019-03-21 23:30:03 +0100

    Set Nibbler refresh time to 0.6.

--- games/nibbler/src/Nibbler.cpp
+++ games/nibbler/src/Nibbler.cpp
@@ -15,7 +15,7 @@ arc::IGame *gameEntryPoint(int &ac, char **av)
 	return new arc::Nibbler(ac, av);
 }
 
-const float arc::Nibbler::_actionTime = 1.0;
+const float arc::Nibbler::_actionTime = 0.6;
 
 arc::Nibbler::Nibbler(int &, char **av) :
 	_score(0),
