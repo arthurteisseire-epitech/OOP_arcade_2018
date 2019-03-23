8dc868874bf00c35246f9cbc9484fa5b73fe62d0
arthur <arthur.teisseire@epitech.eu>
2019-03-23 19:40:59 +0100

    Add qix directory with empty class

---
+++ games/qix/src/Qix.cpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** Qix.cpp
+*/
+
+#include "Qix.hpp"
+
---
+++ games/qix/src/Qix.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** Qix.hpp
+*/
+
+#ifndef ARCADE_QIX_HPP
+#define ARCADE_QIX_HPP
+
+#include "IGame.hpp"
+
+namespace arc {
+	class Qix : public IGame {
+	};
+}
+
+#endif
