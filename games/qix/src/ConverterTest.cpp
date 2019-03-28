b77bc5aeea8e8cfcb7f451733a2a2612fe8fb2e5
Arthamios <adrien.fabre@epitech.eu>
2019-03-28 20:18:07 +0100

    Fixed ConverterTest

--- games/qix/src/ConverterTest.cpp
+++ games/qix/src/ConverterTest.cpp
@@ -12,22 +12,22 @@
 namespace arc {
 	TEST(ConverterTest, posToPercent)
 	{
-		auto pos = Converter::PosToPercent(Position(0, 1), 10, 10);
+		auto pos = Converter::PosToPercent({0, 1}, {10, 10});
 		std::pair<float, float> expected = {0.0, 0.1};
 		ASSERT_EQ(pos, expected);
 
-		pos = Converter::PosToPercent(Position(0, 1), 20, 20);
+		pos = Converter::PosToPercent({0, 1}, {20, 20});
 		expected = {0.0, 0.05};
 		ASSERT_EQ(pos, expected);
 	}
 
 	TEST(ConverterTest, sizeToPercent)
 	{
-		auto pos = Converter::SizeToPercent(10, 10);
+		auto pos = Converter::SizeToPercent({10, 10});
 		std::pair<float, float> expected = {0.1, 0.1};
 		ASSERT_EQ(pos, expected);
 
-		pos = Converter::SizeToPercent(20, 20);
+		pos = Converter::SizeToPercent({20, 20});
 		expected = {0.05, 0.05};
 		ASSERT_EQ(pos, expected);
 	}
