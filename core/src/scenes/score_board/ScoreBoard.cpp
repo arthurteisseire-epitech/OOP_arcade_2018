8fbcff8757419bc5b34ac834ec6d843882c2ed6e
arthur <arthur.teisseire@epitech.eu>
2019-03-29 10:48:10 +0100

    Add empty ScoreBoard class and add it to core Makefile

---
+++ core/src/scenes/score_board/ScoreBoard.cpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** ScoreBoard.cpp
+*/
+
+#include "ScoreBoard.hpp"
+
---
+++ core/src/scenes/score_board/ScoreBoard.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2018
+** arcade
+** File description:
+** ScoreBoard.hpp
+*/
+
+#ifndef ARCADE_SCOREBOARD_HPP
+#define ARCADE_SCOREBOARD_HPP
+
+#include "Scene.hpp"
+
+namespace arc {
+	class ScoreBoard : public Scene {
+	};
+}
+#endif
