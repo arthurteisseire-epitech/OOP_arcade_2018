607a391440d950885c43e52d8cf2ebbcc02261e0
arthur <arthur.teisseire@epitech.eu>
2019-03-29 11:25:57 +0100

    Add hard-coded rows to ScoreBoard

--- core/src/scenes/score_board/ScoreBoard.cpp
+++ core/src/scenes/score_board/ScoreBoard.cpp
@@ -10,6 +10,9 @@
 arc::ScoreBoard::ScoreBoard(const std::shared_ptr<arc::SharedData> &playerData) :
 	Scene(playerData)
 {
+	_rows.emplace(0, std::make_pair<std::string, std::string>("Gamename", "PlayerName"));
+	_rows.emplace(10, std::make_pair<std::string, std::string>("hello", "hdddddddddddddddddddddd"));
+	createTexts();
 }
 
 void arc::ScoreBoard::update(const std::map<arc::Key, arc::KeyState> &, float)
@@ -18,10 +21,33 @@ void arc::ScoreBoard::update(const std::map<arc::Key, arc::KeyState> &, float)
 
 std::vector<std::reference_wrapper<const arc::IComponent>> arc::ScoreBoard::getComponents() const
 {
-	return std::vector<std::reference_wrapper<const arc::IComponent>>();
+	std::vector<std::reference_wrapper<const arc::IComponent>> wrapper;
+
+	for (const auto &gameName : _gameNames)
+		wrapper.emplace_back(gameName);
+
+	for (const auto &playerName : _playerNames)
+		wrapper.emplace_back(playerName);
+
+	for (const auto &score : _scores)
+		wrapper.emplace_back(score);
+
+	return wrapper;
 }
 
 arc::SCENE arc::ScoreBoard::nextScene(const std::map<arc::Key, arc::KeyState> &keys) const
 {
 	return SCOREBOARD;
+}
+
+void arc::ScoreBoard::createTexts()
+{
+	float y = 0.3f;
+
+	for (const auto &row : _rows) {
+		_gameNames.emplace_back(row.second.first, std::pair<float, float>(0.4, y), 30);
+		_playerNames.emplace_back(row.second.second, std::pair<float, float>(0.6, y), 30);
+		_scores.emplace_back(std::to_string(row.first), std::pair<float, float>(0.8, y), 30);
+		y += 0.05;
+	}
 }
--- core/src/scenes/score_board/ScoreBoard.hpp
+++ core/src/scenes/score_board/ScoreBoard.hpp
@@ -8,6 +8,7 @@
 #ifndef ARCADE_SCOREBOARD_HPP
 #define ARCADE_SCOREBOARD_HPP
 
+#include "Text.hpp"
 #include "Scene.hpp"
 
 namespace arc {
@@ -17,6 +18,14 @@ namespace arc {
 		void update(const std::map<Key, KeyState> &keys, float deltaTime) override;
 		std::vector<std::reference_wrapper<const IComponent>> getComponents() const override;
 		arc::SCENE nextScene(const std::map<Key, KeyState> &keys) const override;
+	private:
+		void createTexts();
+
+		std::map<int, std::pair<std::string, std::string>> _rows;
+		std::vector<Text> _gameNames;
+		std::vector<Text> _playerNames;
+		std::vector<Text> _scores;
 	};
 }
+
 #endif
