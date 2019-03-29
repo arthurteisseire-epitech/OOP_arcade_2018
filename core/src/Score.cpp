a28627be52b566954e23aba4166c4515558d476e
arthur <arthur.teisseire@epitech.eu>
2019-03-29 10:45:36 +0100

    Add score to file instead of erase it each time

--- core/src/Score.cpp
+++ core/src/Score.cpp
@@ -12,8 +12,9 @@ const std::string arc::Score::FILENAME = "scores.txt";
 
 void arc::Score::write(const std::string &gameName, const std::string &playerName, int score)
 {
-	std::ofstream file("scores.txt");
+	std::ofstream file;
 
+	file.open(FILENAME, std::ios::app);
 	file << gameName << " " << playerName << " " << score << std::endl;
 	file.close();
 }
