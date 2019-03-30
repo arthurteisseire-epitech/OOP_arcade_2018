297ca78c6f0dcbf16a0bb4f8d6f790f88e317ca4
arthur <arthur.teisseire@epitech.eu>
2019-03-30 20:25:46 +0100

    Remove cov.sh script

--- cov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOCATION="."
-SRC="CMakeFiles/units.dir/src/"
-find $SRC -name '*.cpp' -exec cp -t $LOCATION {} +
-find $SRC -name '*.gcno' -exec cp -t $LOCATION {} +
-find $SRC -name '*.gcda' -exec cp -t $LOCATION {} +
-cd $LOCATION
-find $SRC -name '*.cpp' -exec gcov -bf {} \;
